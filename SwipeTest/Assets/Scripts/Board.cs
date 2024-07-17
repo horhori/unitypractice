13abfd750f11681ec38f3794085451f41271e9a0
horhori <horhori@naver.com>
2024-07-18 02:13:48 +0900

    매칭 후 새로 생성된 블럭이 아래로 떨어지는거 test

--- SwipeTest/Assets/Scripts/Board.cs
+++ SwipeTest/Assets/Scripts/Board.cs
@@ -6,6 +6,8 @@ public class Board : MonoBehaviour
 {
     public int width;
     public int height;
+    // 새로 생성된 Dot을 떨어뜨리기 위해 offSet만큼 위로 올려서 생성 
+    public int offSet;
     public GameObject tilePrefab;
     public GameObject[] dots;
     private BackgroundTile[,] allTiles;
@@ -25,17 +27,177 @@ public class Board : MonoBehaviour
         {
             for (int j = 0; j < height; j++)
             {
-                Vector2 tempPosition = new Vector2(i, j);
+                Vector2 tempPosition = new Vector2(i, j + offSet);
                 GameObject backgroundTile = Instantiate(tilePrefab, tempPosition, Quaternion.identity);
                 backgroundTile.transform.parent = this.transform;
                 backgroundTile.name = "( " + i + ". " + j + " )";
 
+                // 다시 생성하는 횟수 100까지 하려고 함
+                int maxIterations = 0;
+
                 int dotToUse = Random.Range(0, dots.Length);
+
+                while(MatchesAt(i, j, dots[dotToUse]) && maxIterations < 100)
+                {
+                    dotToUse = Random.Range(0, dots.Length);
+                }
+                maxIterations = 0;
+
                 GameObject dot = Instantiate(dots[dotToUse], tempPosition, Quaternion.identity);
+                dot.GetComponent<Dot>().row = j;
+                dot.GetComponent<Dot>().column = i;
+
                 dot.transform.parent = this.transform;
                 dot.name = "( " + i + ". " + j + " )";
                 allDots[i, j] = dot;
             }
+        }
+    }
+
+    private bool MatchesAt(int column, int row, GameObject piece)
+    {
+        // column이 1보다 크면 왼쪽에 컬럼이 두개 있다는 거기 때문에 왼쪽으로 한번 가서 체크하고 또 한번 왼쪽으로 가서 체크
+        // row도 마찬가지 1보다 크면 아래에 로우가 두개 있다는 뜻이기 때문에 아래로 한번 가서 체크하고 한번 더 아래로 체크
+        // 세팅 시에 미리 매칭이 일어나지 않도록 하기 위함
+        if (column > 1 && row > 1)
+        {
+            if (allDots[column - 1, row].tag==piece.tag && allDots[column - 2, row].tag == piece.tag)
+            {
+                return true;
+            }
+
+            if (allDots[column, row -1].tag == piece.tag && allDots[column, row-2].tag == piece.tag)
+            {
+                return true;
+            }
+        } 
+        // 위 조건만 있으면 0, 1 컬럼과 0, 1 로우에서 매칭 발생함. 해당 컬럼 로우에 대한 조건
+        else if (column <= 1 || row <= 1)
+        {
+            if (row > 1)
+            {
+                if (allDots[column, row - 1].tag == piece.tag && allDots[column, row - 2].tag == piece.tag)
+                {
+                    return true;
+                }
+            }
+
+            if (column > 1)
+            {
+                if (allDots[column - 1, row].tag == piece.tag && allDots[column -2, row].tag == piece.tag)
+                {
+                    return true;
+                }
+            }
+
+        }
+
+        return false;
+    }
+
+    // isMatched true로 체크된 Dot Destroy
+    private void DestroyMatchesAt(int column, int row)
+    {
+        if (allDots[column, row].GetComponent<Dot>().isMatched)
+        {
+            Destroy(allDots[column, row]);
+            allDots[column, row] = null;
+        }
+    }
+
+    public void DestroyMatches()
+    {
+        for (int i=0; i<width; i++)
+        {
+            for (int j=0; j<height; j++)
+            {
+                if (allDots[i, j] != null)
+                {
+                    DestroyMatchesAt(i, j);
+                }
+            }
+        }
+        StartCoroutine(DecreaseRowCo());
+    }
+
+    // coroutine으로 빈 자리 row를 감소
+    private IEnumerator DecreaseRowCo()
+    {
+        int nullCount = 0;
+        for (int i=0; i<width; i++)
+        {
+            for (int j=0; j<height; j++)
+            {
+                if (allDots[i, j] == null)
+                {
+                    nullCount++;
+                } else if(nullCount > 0)
+                {
+                    allDots[i, j].GetComponent<Dot>().row -= nullCount;
+                    allDots[i, j] = null;
+                }
+                
+            }
+            nullCount = 0;
+        }
+
+        yield return new WaitForSeconds(.4f);
+        StartCoroutine(FillBoardCo());
+    }
+
+    // 매칭 후 새로 채우기
+    // 1. Dot 새로 생성
+
+    private void RefillBoard()
+    {
+        for (int i=0; i<width; i++)
+        {
+            for (int j=0; j<height; j++)
+            {
+                if (allDots[i,j] == null)
+                {
+                    // 생성되는 곳 y에 offSet만큼 더함
+                    // 
+                    Vector2 tempPosition = new Vector2(i, j + offSet);
+                    int dotToUse = Random.Range(0, dots.Length);
+                    GameObject piece = Instantiate(dots[dotToUse], tempPosition, Quaternion.identity);
+                    allDots[i, j] = piece;
+                    piece.GetComponent<Dot>().row = j;
+                    piece.GetComponent<Dot>().column = i;
+                }
+            }
+        }
+    }
+
+    // 2. 생성 후 매칭 체크
+    private bool MatchesOnBoard()
+    {
+        for (int i=0; i<width; i++)
+        {
+            for (int j=0; j<height; j++)
+            {
+                if (allDots[i, j] != null)
+                {
+                    if (allDots[i, j].GetComponent<Dot>().isMatched)
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private IEnumerator FillBoardCo()
+    {
+        RefillBoard();
+        yield return new WaitForSeconds(.5f);
+
+        while(MatchesOnBoard())
+        {
+            yield return new WaitForSeconds(.5f);
+            DestroyMatches();
         }
     }
 }
--- SwipeTest/Assets/Scripts/Dot.cs
+++ SwipeTest/Assets/Scripts/Dot.cs
@@ -24,23 +24,28 @@ public class Dot : MonoBehaviour
     [SerializeField]
     private float swipeAngle = 0;
 
+    // 쉽게 스와이프되는거 방지용
+    public float swipeResist = 1f;
+
     // Start is called before the first frame update
     void Start()
     {
         board = FindObjectOfType<Board>();
-        targetX = (int)transform.position.x;
-        targetY = (int)transform.position.y;
-        row = targetY;
-        column = targetX;
-        // 맨 처음의 row, column을 기억하기 위해 사용
-        previousRow = row;
-        previousColumn = column;
+        //targetX = (int)transform.position.x;
+        //targetY = (int)transform.position.y;
+        //row = targetY;
+        //column = targetX;
+        //// 맨 처음의 row, column을 기억하기 위해 사용
+        //previousRow = row;
+        //previousColumn = column;
     }
 
     // Update is called once per frame
     void Update()
     {
         FindMatches();
+
+        // 색깔 검정으로 바뀌는건 isMatched 되서임
         if (isMatched)
         {
             SpriteRenderer mySprite = GetComponent<SpriteRenderer>();
@@ -50,34 +55,42 @@ public class Dot : MonoBehaviour
         targetX = column;
         targetY = row;
 
-        // X 방향 스왑
+        // X축 이동
+        // Dot이 원래 위치하고 있어야 할 x, y 제자리에 없으면 원래대로 이동함
         if (Mathf.Abs(targetX - transform.position.x) > .1)
         {
             // Move Towards the target
             tempPosition = new Vector2(targetX, transform.position.y);
             transform.position = Vector2.Lerp(transform.position, tempPosition, .4f);
+            if (board.allDots[column, row] != this.gameObject)
+            {
+                board.allDots[column, row] = this.gameObject;
+            }
         }
         else
         {
             // Directly set the position
             tempPosition = new Vector2(targetX, transform.position.y);
             transform.position = tempPosition;
-            board.allDots[column, row] = this.gameObject;
+
         }
 
-        // Y 방향 스왑
+        // Y축 이동
         if (Mathf.Abs(targetY - transform.position.y) > .1)
         {
             // Move Towards the target
             tempPosition = new Vector2(transform.position.x, targetY);
             transform.position = Vector2.Lerp(transform.position, tempPosition, .4f);
+            if (board.allDots[column, row] != this.gameObject)
+            {
+                board.allDots[column, row] = this.gameObject;
+            }
         }
         else
         {
             // Directly set the position
             tempPosition = new Vector2(transform.position.x, targetY);
             transform.position = tempPosition;
-            board.allDots[column, row] = this.gameObject;
         }
     }
 
@@ -85,17 +98,24 @@ public class Dot : MonoBehaviour
     public IEnumerator CheckMoveCo()
     {
         yield return new WaitForSeconds(.5f);
+
         if (otherDot != null)
         {
             if(!isMatched && !otherDot.GetComponent<Dot>().isMatched)
+            // match되지 않으면 다시 되돌아감
             {
                 otherDot.GetComponent<Dot>().row = row;
                 otherDot.GetComponent<Dot>().column = column;
                 row = previousRow;
                 column = previousColumn;
             }
+            else
+            // match되면 Destroy 로직 실행
+            {
+                board.DestroyMatches();
+            }
             otherDot = null;
-        }
+        } 
           
     } 
 
@@ -118,8 +138,13 @@ public class Dot : MonoBehaviour
         // -45도 ~ 45도 = 오른쪽
         // -135도 ~ -45도 = 아래
         // -135도 ~ 135도 = 왼쪽
-        swipeAngle = Mathf.Atan2(finalTouchPosition.y - firstTouchPosition.y, finalTouchPosition.x - firstTouchPosition.x) * 180 / Mathf.PI;
-        Debug.Log("swipeAngle : " + swipeAngle);
+
+        // swipeResist 이상의 입력을 했을 때만 적용인데 잘 안먹는거 같은데..
+        if (Mathf.Abs(finalTouchPosition.y - firstTouchPosition.y) > swipeResist || Mathf.Abs(finalTouchPosition.x - firstTouchPosition.x) > swipeResist)
+        {
+            swipeAngle = Mathf.Atan2(finalTouchPosition.y - firstTouchPosition.y, finalTouchPosition.x - firstTouchPosition.x) * 180 / Mathf.PI;
+        }
+
         MovePieces();
     }
 
@@ -130,6 +155,8 @@ public class Dot : MonoBehaviour
         {
             // Right Swipe
             otherDot = board.allDots[column + 1, row];
+            previousRow = row;
+            previousColumn = column;
             otherDot.GetComponent<Dot>().column -= 1;
             column += 1;
         } 
@@ -137,6 +164,8 @@ public class Dot : MonoBehaviour
         {
             // Up Swipe
             otherDot = board.allDots[column, row + 1];
+            previousRow = row;
+            previousColumn = column;
             otherDot.GetComponent<Dot>().row -= 1;
             row += 1;
         }
@@ -144,6 +173,8 @@ public class Dot : MonoBehaviour
         {
             // Left Swipe
             otherDot = board.allDots[column - 1, row];
+            previousRow = row;
+            previousColumn = column;
             otherDot.GetComponent<Dot>().column += 1;
             column -= 1;
         }
@@ -151,36 +182,49 @@ public class Dot : MonoBehaviour
         {
             // Down Swipe
             otherDot = board.allDots[column, row -1];
+            previousRow = row;
+            previousColumn = column;
             otherDot.GetComponent<Dot>().row += 1;
             row -= 1;
         }
         StartCoroutine(CheckMoveCo());
     }
 
+    // 여기서 Dot들을 isMatched true로 바꿈
+    // tag를 바꾸지 않아서 에러나는듯 함
+    // 가운데 기준으로 좌우, 위아래 tag가 같으면 셋 다 true로 바꿈
     void FindMatches()
     {
         if (column > 0 && column < board.width - 1)
         {
             GameObject leftDot1 = board.allDots[column - 1, row];
             GameObject rightDot1 = board.allDots[column + 1, row];
-            if (leftDot1.tag == this.gameObject.tag && rightDot1.tag == this.gameObject.tag)
+            if(leftDot1 != null &&  rightDot1 != null)
             {
-                leftDot1.GetComponent<Dot>().isMatched = true;
-                rightDot1.GetComponent<Dot>().isMatched = true;
-                isMatched = true;
+                if (leftDot1.tag == this.gameObject.tag && rightDot1.tag == this.gameObject.tag)
+                {
+                    leftDot1.GetComponent<Dot>().isMatched = true;
+                    rightDot1.GetComponent<Dot>().isMatched = true;
+                    isMatched = true;
+                }
             }
+
         }
 
         if (row > 0 && row < board.height - 1)
         {
             GameObject upDot1 = board.allDots[column, row + 1];
             GameObject downDot1 = board.allDots[column, row - 1];
-            if (upDot1.tag == this.gameObject.tag && downDot1.tag == this.gameObject.tag)
+            if (upDot1 != null && downDot1 != null)
             {
-                upDot1.GetComponent<Dot>().isMatched = true;
-                downDot1.GetComponent<Dot>().isMatched = true;
-                isMatched = true;
+                if (upDot1.tag == this.gameObject.tag && downDot1.tag == this.gameObject.tag)
+                {
+                    upDot1.GetComponent<Dot>().isMatched = true;
+                    downDot1.GetComponent<Dot>().isMatched = true;
+                    isMatched = true;
+                }
             }
+
         }
     }
 }
